c4f2cc370c97321051c8a8fb70c360bace434936
oriolfernandezpizarro-sudo <oriol.fernandez.pizarro@estudiantat.upc.edu>
2026-04-20 17:18:47 +0200

    version 2

--- aircraft..py
+++ aircraft..py
@@ -49,3 +49,57 @@ def LoadArrivals(filename):
         return []
 
     return arrivals
+
+def SaveFlights(aircrafts, filename):
+    if not aircrafts:
+        print("Error: lista vacía")
+        return -1
+
+    file = open(filename, "w")
+
+    for a in aircrafts:
+        aircraft_id = a.aircraft_id if a.aircraft_id != "" else "-"
+        origin = a.origin_airport if a.origin_airport != "" else "-"
+        time = a.time_of_landing if a.time_of_landing != "" else "-"
+        airline = a.airline_id if a.airline_id != "" else "-"
+
+        line = f"{aircraft_id} {origin} {time} {airline}\n"
+        file.write(line)
+
+    file.close()
+    return 0
+
+
+def MapFlights(aircrafts):
+    if not aircrafts:
+        print("Error: lista vacía")
+        return
+
+    airports = LoadAirports("Airports.txt")
+
+    if "LEBL" not in airports:
+        print("Error: LEBL no encontrado")
+        return
+
+    lebl = airports["LEBL"]
+
+    file = open("flights.kml", "w")
+
+    file.write("""<?xml version="1.0" encoding="UTF-8"?> <kml xmlns="http://www.opengis.net/kml/2.2"> <Document>""")
+
+    for a in aircrafts:
+        if a.origin_airport not in airports:
+            continue
+
+        origin = airports[a.origin_airport]
+
+        file.write(f""" <Placemark> <name>{a.aircraft_id}</name> <LineString> <coordinates> {origin[1]},{origin[0]},0 {lebl[1]},{lebl[0]},0 </coordinates> </LineString> </Placemark> """)
+
+    file.write("""
+</Document>
+</kml>
+""")
+
+    file.close()
+
+    print("Archivo flights.kml generado ")
